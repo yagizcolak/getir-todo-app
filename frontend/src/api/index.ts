59d59999ffb6133312bec45ec4c4f062a8abbdf6
Yağız Çolak <yagizcolak98@gmail.com>
2024-11-20 22:36:06 +0300

    .env files created for development & production, NORMALLY WOULDN'T PUSH THESE OF COURSE

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api', // Adjust the baseURL as needed
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api',
 });
 
 export default api;
